9471d896481699310fadb51fb263301990659cbe
SRAKIB17 <rakibulssc5@gmail.com>
2024-03-04 21:36:43 +0600

    robiul

--- js.js
+++ js.js
@@ -9,4 +9,5 @@ student_role01 = {
     "prome": 2,
     "zinia": 3
 }
+
 console.log({ ...student_role, ...student_role01 })
--- test.py
+++
@@ -1,4 +0,0 @@
-import collections as cl
-from pprint import pprint as print
-
-print(dir(cl.Counter.copy()))
